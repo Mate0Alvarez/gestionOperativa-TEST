fd36a28ab70664ac25cb3478448d92fddfe66082
Mate0Alvarez <mateo.j.alvarez@gmail.com>
2021-02-04 16:47:15 -0300

    solución 2.0

--- PHP/script.php
+++ PHP/script.php
@@ -1,7 +1,15 @@
 <?php
+// Establecemos un limite de tiempo (0) para que el script no falle en caso de tomarse demasiado tiempo
+set_time_limit(0);
+
 // DECLARAMOS EL SITIO Y EL VENDEDOR
 $SITE_ID = 'MLA';
 $SELLER_ID = 179571326;
+// Corroboramos si el usuario nos ha proporcionado la información
+if(isset($_POST)){
+
+$SITE_ID = $_POST["site_id"];
+$SELLER_ID = $_POST["seller_id"];
 
 // // REALIZAMOS LA PETICIÓN
 $curl = curl_init();
@@ -15,18 +23,74 @@ $RESULTS = "[$RESULTS]";
 $RESULTS = json_decode($RESULTS,True);
 // DECLARAMOS LA VARIABLE PRODUCTS CON LOS PRODUCTOS ENCONTRADOS
 $PRODUCTS = $RESULTS[0]["results"];
+// AVERIGUAMOS CUANTOS PRODUCTOS HAY EN TOTAL
+$PAGING = $RESULTS[0]["paging"]["total"];
 // CREAMOS UN ARRAY VACÍO DONDE LUEGO EXPORTAREMOS LA INFORMACIÓN
 $PRODUCTS_LOG = [];
 
-// RECORREMOS LOS PRODUCTOS Y TOMAMOS LA INFORMACIÓN SOLICITADA
+// RECORREMOS LOS PRIMEROS PRODUCTOS OBTENIDOS Y TOMAMOS LA INFORMACIÓN SOLICITADA
 foreach( $PRODUCTS AS $PRODUCT){
+	// Buscamos el NAME de la categoría
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, "https://api.mercadolibre.com/categories/".$PRODUCT['category_id']."");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$RESULTS = curl_exec($curl);
+	curl_close($curl);
+	// AGREMAOS CORCHETES PARA TRANSFORMARLO EN JSON
+	$RESULTS = "[$RESULTS]";
+	// CONVERTIMOS LOS RESULTADOS EN ARRAY
+	$RESULTS = json_decode($RESULTS,True);
+	// DECLARAMOS LA VARIABLE NAME
+	$NAME = $RESULTS[0]["name"];
+	// INSERTAMOS TODOS LOS TADOS EN EL ARRAY
 	$PRODUCTS_LOG[]= [
 		"Id del ítem" => $PRODUCT["id"],
 		"Título del ítem" => $PRODUCT["title"],
 		"Categoría_ID donde está publicado" => $PRODUCT["category_id"],
-		"Nombre de la categoría" => $PRODUCT["domain_id"]
+		"Nombre de la categoría" => $NAME
 	];
 };
+
+// DECLARAMOS LA VARIABLE OFFSET
+$offset = 50;
+for ($i=0; $i < ($PAGING/50); $i++) {
+	// REPETIMOS PROCESO DE BUSQUEDA AUMENTANDO EL OFFSET
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, "https://api.mercadolibre.com/sites/$SITE_ID/search?seller_id=$SELLER_ID&offset=$offset");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$RESULTS = curl_exec($curl);
+	curl_close($curl);
+	// AGREMAOS CORCHETES PARA TRANSFORMARLO EN JSON
+	$RESULTS = "[$RESULTS]";
+	// CONVERTIMOS LOS RESULTADOS EN ARRAY
+	$RESULTS = json_decode($RESULTS,True);
+	// DECLARAMOS LA VARIABLE PRODUCTS CON LOS PRODUCTOS ENCONTRADOS
+	$PRODUCTS = $RESULTS[0]["results"];
+
+	// RECORREMOS LOS PRODUCTOS OBTENIDOS Y TOMAMOS LA INFORMACIÓN SOLICITADA
+	foreach( $PRODUCTS AS $PRODUCT){
+		// Buscamos el NAME de la categoría
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, "https://api.mercadolibre.com/categories/".$PRODUCT['category_id']."");
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$RESULTS = curl_exec($curl);
+		curl_close($curl);
+		// AGREGAMOS CORCHETES PARA TRANSFORMARLO EN JSON
+		$RESULTS = "[$RESULTS]";
+		// CONVERTIMOS LOS RESULTADOS EN ARRAY
+		$RESULTS = json_decode($RESULTS,True);
+		// DECLARAMOS LA VARIABLE NAME
+		$NAME = $RESULTS[0]["name"];
+		// INSERTAMOS TODOS LOS TADOS EN EL ARRAY $PRODUCTS_LOG
+		$PRODUCTS_LOG[]= [
+			"Id del ítem" => $PRODUCT["id"],
+			"Título del ítem" => $PRODUCT["title"],
+			"Categoría_ID donde está publicado" => $PRODUCT["category_id"],
+			"Nombre de la categoría" => $NAME
+		];
+	};
+	$offset = $offset+50;
+}
 
 // EXPORTAMOS
 // NO OLVIDAR JSON_UNESCAPED_UNICODE PARA EVITAR ERRORES DE FORMATO CON Ñ Y TILDES
@@ -35,25 +99,9 @@ $PRODUCTS_JSON = json_encode($PRODUCTS_LOG,JSON_UNESCAPED_UNICODE);
 $file = 'productos.json';
 // SOBRESCRIBIMOS
 file_put_contents($file, $PRODUCTS_JSON);
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Test Gestión Operativa</title>
-  </head>
-  <body>
-		<?php
-		$i = 1;
 
-		foreach ($PRODUCTS_LOG as $PRODUCTS){?>
-			<div class="">
-				<h2>Producto n°<?php echo $i?> :</h2>
-				<p><b>El Id del item es: </b><?php echo $PRODUCTS["Id del ítem"]?></p>
-				<p><b>El título del item es: </b><?php echo $PRODUCTS["Título del ítem"]?></p>
-				<p><b>El category_ID donde está publicado: </b><?php echo $PRODUCTS["Categoría_ID donde está publicado"]?></p>
-				<p><b>El nombre de la categoría es: </b><?php echo $PRODUCTS["Nombre de la categoría"]?></p>
-			</div>
-		<?php $i=$i+1;}; ?>
-  </body>
-</html>
+//RESPONDEMOS LA PETICIÓN
+$response = "Se ha creado el archivo correctamente";
+echo json_encode("Se ha creado el archivo log exitosamente");
+};
+?>
